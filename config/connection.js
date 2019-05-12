fc046edbdb31e3fc0790771a03d7de1f24a856e6
Taylor <Taylor@Taylors-MacBook-Pro-2.local>
2019-05-12 18:27:13 -0400

    jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,16 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
         host: "localhost",
-        port: 8889,
         user: "root",
         password: "root",
         database: "burgers_db"
-    });
+})};
 
-connection.connect(function(err) {
-    if(err) {
-        console.log("error connecting: " + err);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-})
+connection.connect();
 
 module.exports = connection;
